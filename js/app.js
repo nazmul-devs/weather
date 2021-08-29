468c25acc1124122e3cd5023aebdd015004cf853
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-08-29 17:19:05 +0600

    complite with javascript weather map

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
-
+// get input value
 const inputValue = () => {
     const value = document.getElementById('search-input');
-    console.log(value.value);
+    loadData(value.value);
     value.value = '';
+};
+// get API response
+const loadData = async city => {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=405b86887d916f5ece71ad66adf9d2ef`);
+    const data = await res.json();
+    updateWeather(data);
+};
 
+const updateWeather = weather => {
+    const weatherContainer = document.getElementById('weather');
+    const feelsLike = Math.round(weather.main.feels_like / 8.587);
+    const temp = weather.main.temp / 8.587;
+    const country = (country) => {
+        if (country == 'BD') {
+        return 'Bangladesh';
+    } else {
+        return country;
+    }
+  }
+    weatherContainer.innerHTML = `
+            <h2>${weather.name}</h2>
+            <h3>${country(weather.sys.country)}</h3>
+            <h3>${temp.toFixed(1)}° C</h3>
+            <h3>Feels like ${feelsLike}° C</h3>
+            <h4>Clouds</h4>
+    `;
 }
